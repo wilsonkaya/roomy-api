5712f0e4270fa4857425a2bef7cedab075e186f3
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-03-28 10:03:15 -0400

    Re format index controller
    Index of review is returns only reviews related to rentals
    
    Tested both in backend and front-end and it is working
    next is to create reviews

--- app/controllers/reviews.js
+++ app/controllers/reviews.js
@@ -9,7 +9,8 @@ const setUser = require('./concerns/set-current-user');
 const setModel = require('./concerns/set-mongoose-model');
 
 const index = (req, res, next) => {
-  Review.find()
+  console.log(req.query)
+  Review.find({ rentals: req.query.rentals })
     .then(reviews => res.json({
       reviews: reviews.map((e) =>
         e.toJSON({ virtuals: true, user: req.user })),
